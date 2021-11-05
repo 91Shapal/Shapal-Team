e96595964d95f48f7bd9c8f2fdb7538210744d29
91Shapal <93367065+91Shapal@users.noreply.github.com>
2021-11-05 11:45:31 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <html>
 <body>
+   .button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+   <a href="#" class="button">Link Button</a>
    <h1 style="background-color:Tomato;">Shapal</h1>
    <video width="700" height="640" controls>
   <source src="https://www.youtube.com/watch?v=vI6yG78jPnk" type="video/mp4">
