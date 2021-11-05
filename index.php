ce5eb548242b8d47442256fd406120e8b7113c92
91Shapal <93367065+91Shapal@users.noreply.github.com>
2021-11-05 09:18:53 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <html>
 <body>
-  
+   <video width="320" height="240" controls>
+  <source src="movie.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+</video> 
 <h1 style="background-color:Tomato;">Shapal</h1>
 <h2>HTML Image</h2>
 <img src="pic_trulli.jpg" alt="Trulli" width="500" height="333">
